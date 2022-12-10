50b050b520fe3d1cc0160f12617064279ee8f2e3
Post-Apocalypse <postapocalypsecore@gmail.com>
2022-12-10 20:12:36 +0600

    app init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Cloud Gallery
+> Author's cloud gallery for fast loading and comfortable viewing of images with an authorization system, photo compression and smart tag search
+### Under Development
---
+++ vite.config.ts
@@ -0,0 +1,44 @@
+import { fileURLToPath, URL } from 'node:url'
+import { VitePWA } from 'vite-plugin-pwa'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'safari-pinned-tab.svg'],
+      manifest: {
+        name: 'Cloud Gallery',
+        short_name: 'Cloud Gallery',
+        description: 'Gallery for fast loading and comfortable viewing of images',
+        theme_color: '#242424',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
